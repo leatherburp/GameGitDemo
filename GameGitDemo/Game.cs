7ebe78cb79e2bafae602e3e6c67a4df6dcdf2911
leatherburp <nsmith3@colum.edu>
2021-04-22 14:09:27 -0500

    Created skeleton of Game class with Run method

---
+++ GameGitDemo/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GameGitDemo
+{
+    public class Game
+    {
+        public void Run()
+        {
+            Console.WriteLine("=== Welcome to the game ===");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
