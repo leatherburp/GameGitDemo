7d0f873a58b8c85c753032274e9fdb3757230b18
leatherburp <nsmith3@colum.edu>
2021-04-22 14:16:27 -0500

    Refactored Game to bring in System.Console namespace

--- GameGitDemo/Game.cs
+++ GameGitDemo/Game.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using static System.Console;
 namespace GameGitDemo
 {
     public class Game
     {
         public void Run()
         {
-            Console.WriteLine("=== Welcome to the game ===");
+            WriteLine("=== Welcome to the game ===");
 
-            Console.WriteLine("\nPress any key to exit...");
-            Console.ReadKey(true);
+            WriteLine("\nPress any key to exit...");
+            ReadKey(true);
         }
     }
 }
